35059bdc09a568abc382b312f7dd3f3d02f01295
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2020-10-12 12:05:41 +0530

    c

---
+++ Graphs/Network_delay.cpp
@@ -0,0 +1,18 @@
+/*
+
+Link : https://leetcode.com/problems/network-delay-time/
+
+Problem Statement :
+There are N network nodes, labelled 1 to N.
+
+Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node, and w is the time it takes for a signal to travel from source to target.
+
+Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
+
+
+
+Example 1:
+
+Input: times = [[2,1,1],[2,3,1],[3,4,1]], N = 4, K = 2
+Output: 2
+*/
