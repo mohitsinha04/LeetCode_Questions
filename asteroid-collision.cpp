14c81d95fdd2c61fea1865db2abd4b6b031eae15
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2020-11-26 23:57:06 +0530

    working commit - Created using LeetHub

---
+++ asteroid-collision.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& nums) {
+        if(nums.size() == 0) return {};
+        stack<int> stack;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0 || stack.empty()) stack.push(nums[i]);
+            else {
+                while(!stack.empty() && stack.top() > 0 && stack.top() < abs(nums[i])) stack.pop();
+                if(stack.empty() || stack.top() < 0) stack.push(nums[i]);
+                else if(stack.top() == abs(nums[i])) stack.pop();
+            }
+        }
+        nums = {};
+        while(!stack.empty()) {
+            nums.push_back(stack.top());
+            stack.pop();
+        }
+        reverse(nums.begin(), nums.end());
+        return nums;
+    }
+};
