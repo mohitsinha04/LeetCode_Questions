5c2df537d06a1573c4fc2e50f5aaa9db8d93102c
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2020-12-11 15:45:14 +0530

    working commit - Created using LeetHub

---
+++ boundary-of-binary-tree/boundary-of-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void addleft(TreeNode* root, vector<int>& res) {
+        if(root == nullptr) return;
+        if(root->left) {
+            res.push_back(root->val);
+            addleft(root->left, res);
+        } else if(root->right) {
+            res.push_back(root->val);
+            addleft(root->right, res);
+        }
+    }
+    void addleaves(TreeNode* root, vector<int>& res) {
+        if(!root) return;
+        if(!root->left && !root->right) {
+            res.push_back(root->val);
+            return;
+        }
+        addleaves(root->left, res);
+        addleaves(root->right, res);
+    }
+    void addright(TreeNode* root, vector<int>& res) {
+        if(!root) return;
+        if(root->right) {
+            addright(root->right, res);
+            res.push_back(root->val);
+        } else if(root->left) {
+            addright(root->left, res);
+            res.push_back(root->val);
+        }
+    }
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if(!root) return {};
+        vector<int> res;
+        res.push_back(root->val);
+        if(root->left) addleft(root->left, res);
+        if(root->left || root->right)
+            addleaves(root, res);
+        if(root->right) addright(root->right, res);
+        return res;
+    }
+};
