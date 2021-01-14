1ee417d366c43f0d42c4d6361fd17b749c83bbc0
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2021-01-14 13:27:08 +0530

    Time: 20 ms (99.97%), Space: 11.8 MB (100.00%) - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int count = 1;
+        pq.push(intervals[0][1]);
+        for(int i = 1; i < intervals.size(); i++) {
+            if(pq.top() <= intervals[i][0]) pq.pop();
+            pq.push(intervals[i][1]);
+            count = max(count, (int)pq.size());
+        }
+        return count;
+    }
+};
