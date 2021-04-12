9dbac00977171dac996d4c7e1254e82a9c38b0d6
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2021-04-12 09:13:19 +0530

    Time: 8 ms (90.05%), Space: 10.5 MB (59.55%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> tails(nums.size(), 0);
-        int size = 0;
-        for (int x : nums) {
-            int i = 0, j = size;
-            while (i != j) {
-                int m = (i + j) / 2;
-                if (tails[m] < x)
-                    i = m + 1;
-                else
-                    j = m;
-            }
-            tails[i] = x;
-            if (i == size) ++size;
-        }
-        return size;
-    }
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.empty()) { return 0; } 
+        vector<int> tail;  // keep tail value of each possible len
+        tail.push_back(nums[0]);
+        for (auto n : nums) {
+            if (n <= tail[0]) {
+                tail[0] = n;
+            } else if (n > tail.back()) { // large than the tail of current largest len 
+                tail.push_back(n);
+            } else {  // find smallest one which is >= n
+                int left = 0; 
+                int right = tail.size()-1;
+                int res = left;
+                while(left <= right) {
+                    int mid = left + (right-left)/2;
+                    if (tail[mid] >= n) {
+                        res = mid;
+                        right = mid-1;
+                    } else { // tail[mid] < n
+                        left = mid+1;
+                    }
+                }
+                tail[res] = n;
+            }
+        }
+        return tail.size();
+    }
 };
