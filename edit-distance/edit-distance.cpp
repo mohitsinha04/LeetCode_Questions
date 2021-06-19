688f40f3b30ed26a04f682e41d088aac9ead3e69
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2021-06-19 21:07:07 +0530

    Time: 16 ms (45.26%), Space: 9.1 MB (23.41%) - LeetHub

--- edit-distance/edit-distance.cpp
+++ edit-distance/edit-distance.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int helper(string word1, string word2, int i, int j, vector<vector<int>>& dp) {
-        if(i >= word1.size()) return word2.size() - j;
-        if(j >= word2.size()) return word1.size() - i;
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        if(word1[i] == word2[j]) {
-            dp[i][j] = helper(word1, word2, i + 1, j + 1, dp);
-        }
-        else 
-            dp[i][j] = 1 + min(helper(word1, word2, i + 1, j, dp), 
-                               min(helper(word1, word2, i, j + 1, dp), 
-                                   helper(word1, word2, i + 1, j + 1, dp)));
-        return dp[i][j];
-    }
-    
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), - 1));
-        return helper(word1, word2, 0, 0, dp);
+        if (word1 == word2) return 0;
+        vector<vector<int>> dp (word1.size() + 1, vector<int>(word2.size() + 1));
+        for(int i = 0; i < dp[0].size(); i++)
+            dp[0][i] = i;
+        for(int i = 0; i < dp.size(); i++)
+            dp[i][0] = i;
+        for (int i = 1; i <= word1.size(); i++) {
+            for (int j = 1; j <= word2.size(); j++) {
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
+
+/*1. w == w
+2. w + 1 == w + 1
+3. w + 1 == w
+4. w == w + 1*/
