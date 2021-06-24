82110af6efd51c7cf9623767839545f3b89d1def
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2021-06-24 10:23:30 +0530

    Time: 448 ms (10.14%), Space: 175.7 MB (5.25%) - LeetHub

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -1,41 +1,41 @@
 class LRUCache {
 public:
-    list<int> recent;
-    unordered_map<int, int> cache;
-    unordered_map<int, list<int>::iterator> position;
-    int capacity;
-    
-    void use(int key) {
-        if(position.count(key)) {
-            recent.erase(position[key]);
-        } else if(recent.size() >= capacity) {
-            int old = recent.back();
-            position.erase(old);
-            recent.pop_back();
-            cache.erase(old);
-        }
-        recent.push_front(key);
-        position[key] = recent.begin();
-    }
-    
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-    }
-    
-    int get(int key) {
-        if(cache.count(key)) {
-            use(key);
-            return cache[key];
-        }
-        return -1;
-    }
-    
-    void put(int key, int value) {
-        use(key);
-        cache[key] = value;
-    }
+    list<int> recent;
+    unordered_map<int, int> cache;
+    unordered_map<int, list<int>::iterator> position;
+    int capacity;
+    
+    void use(int key) {
+        if(position.count(key)) {
+            recent.erase(position[key]);
+        } else if(recent.size() >= capacity) {
+            int old = recent.back();
+            position.erase(old);
+            recent.pop_back();
+            cache.erase(old);
+        }
+        recent.push_front(key);
+        position[key] = recent.begin();
+    }
+    
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(cache.count(key)) {
+            use(key);
+            return cache[key];
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        use(key);
+        cache[key] = value;
+    }
 };
-​
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
