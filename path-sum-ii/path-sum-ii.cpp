506d9dcc21662e0e08042cc2da5f39c75fcf757f
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2021-06-24 14:54:08 +0530

    Time: 8 ms (82.41%), Space: 19.9 MB (79.39%) - LeetHub

--- path-sum-ii/path-sum-ii.cpp
+++ path-sum-ii/path-sum-ii.cpp
@@ -1,32 +1,32 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        vector<vector<int>> res;
-        vector<int> curr;
-        backtrack(res, curr, root, sum);
-        return res;
-    }
-    void backtrack(vector<vector<int>>& res, vector<int>& curr, TreeNode* root, int sum){
-        if(root == nullptr) return;
-        curr.push_back(root->val);
-        if(root->left == nullptr && root->right == nullptr && sum == root->val){
-            res.push_back(curr);
-            curr.pop_back();
-            return;
-        }
-        else{
-            backtrack(res, curr, root->left, sum - root->val);
-            backtrack(res, curr, root->right, sum - root->val);
-        }
-        curr.pop_back();
-    }
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        backtrack(res, curr, root, sum);
+        return res;
+    }
+    void backtrack(vector<vector<int>>& res, vector<int>& curr, TreeNode* root, int sum){
+        if(root == nullptr) return;
+        curr.push_back(root->val);
+        if(root->left == nullptr && root->right == nullptr && sum == root->val){
+            res.push_back(curr);
+            curr.pop_back();
+            return;
+        }
+        else{
+            backtrack(res, curr, root->left, sum - root->val);
+            backtrack(res, curr, root->right, sum - root->val);
+        }
+        curr.pop_back();
+    }
 };
