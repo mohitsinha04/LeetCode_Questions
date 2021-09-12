6a22ea62033d06283e4b894db9925f566b47e60a
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2021-09-12 16:34:39 +0530

    Time: 575 ms (17.36%), Space: 175.9 MB (21.29%) - LeetHub

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -4,26 +4,26 @@ public:
     unordered_map<int, int> cache;
     unordered_map<int, list<int>::iterator> position;
     int capacity;
+    LRUCache(int capacity_) {
+        capacity = capacity_;
+    }
     
     void use(int key) {
-        if(position.count(key)) {
+        if (position.count(key)) {
             recent.erase(position[key]);
-        } else if(recent.size() >= capacity) {
+        }
+        else if (recent.size() >= capacity) {
             int old = recent.back();
             position.erase(old);
-            recent.pop_back();
             cache.erase(old);
+            recent.pop_back();
         }
         recent.push_front(key);
         position[key] = recent.begin();
     }
     
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-    }
-    
     int get(int key) {
-        if(cache.count(key)) {
+        if (cache.count(key)) {
             use(key);
             return cache[key];
         }
